1bcf3cf7bde57b46e517a345f58b894d63b80279
kannan <kannans89@gmail.com>
2025-11-25 14:40:49 +0530

    exceptions

---
+++ CustomExceptionApp/CustomExceptionApp/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+using CustomExceptionApp.Domain;
+
+Account acc1 = new Account(1, "Hemang", 1000);
+
+try
+{// open
+    acc1.Withdraw(2 * 1000);
+    //error
+
+    //close()
+}
+
+catch (EmersonInsufficientFundsException ex)
+{// exepects an object excpetion or subclasess
+    Console.ForegroundColor = ConsoleColor.Red;
+    // Console.WriteLine(ex.StackTrace);
+    Console.WriteLine(ex.Message);
+    Console.ResetColor();
+}
+catch (Exception ex)
+{
+  
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Executes all the time, not matter exception happened or not");
+    PrintInfo(acc1);
+
+    //close(),dispose db connections
+}
+
+
+
+
+
+
+void PrintInfo(Account acc)
+{
+    Console.WriteLine("accno is "+acc.AccountNo);
+    Console.WriteLine("name is " + acc.Name);
+    Console.WriteLine("balance " + acc.Balance);
+}
