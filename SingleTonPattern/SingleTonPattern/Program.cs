773806fd95e77810024f486a999ed24c4548e0dd
kannan <kannans89@gmail.com>
2025-11-25 13:59:52 +0530

    singleton

---
+++ SingleTonPattern/SingleTonPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using SingleTonPattern.Service;
+
+
+DataService sv1 =  DataService.CreateAnInstance();
+
+DataService sv2 = DataService.CreateAnInstance();
+
+
+
+Console.WriteLine(sv1.GetHashCode());
+Console.WriteLine(sv2.GetHashCode());
+
+Console.WriteLine(sv1.GetData());
+Console.WriteLine(sv2.GetData());
---
+++ SingleTonPattern/SingleTonPattern/Service/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTonPattern.Service
+{
+    class DataService
+    {
+        private  static  DataService _bucket=null;
+
+        private DataService() {
+             Console.WriteLine("Data Service created"); 
+        }
+
+        public static  DataService CreateAnInstance() {
+
+            if (_bucket == null) { 
+              _bucket = new DataService();
+            }
+
+            return _bucket;
+        }
+
+
+        public string GetData() {
+
+            
+
+            Random random = new Random();
+           int data= random.Next(100);
+            return "Getting data from outside :" + data + " work done by : "+this.GetHashCode();
+        }
+    }
+}
